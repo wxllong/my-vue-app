c93ad0a58ff24f1b05a18e07f5887a54fa1812be
wuxilong <2278815788@qq.com>
2025-05-30 16:45:52 +0800

    feat: 新增icons组件

---
+++ src/components/ComSvgIcon/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+defineOptions({
+  name: 'ComSvgIcon',
+});
+
+const props = withDefaults(defineProps<{
+  name: string
+  color?: string
+  size?: string | number
+}>(), {
+  color: '#fff',
+});
+
+const symbolId = computed(() => `#icon-${props.name}`);
+const style = computed(() => {
+  return {
+    width: typeof props.size === 'number' ? `${props.size / 16}rem` : props.size,
+    height: typeof props.size === 'number' ? `${props.size / 16}rem` : props.size,
+  };
+});
+</script>
+
+<template>
+  <svg aria-hidden="true" :style="style" class="text-base" :fill="color">
+    <use :href="symbolId" :fill="color" />
+  </svg>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,65 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import path, { dirname, resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        '@vueuse/core'
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/stores',
-        'src/composables'
-      ],
-      eslintrc: {
-        enabled: true
-      }
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-      resolvers: [
-        AntDesignVueResolver({
-          resolveIcons: true,
-          importStyle: false,
-        }),
-      ],
-      dirs: ['src/components']
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia',
+          '@vueuse/core'
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: [
+          'src/stores',
+          'src/composables'
+        ],
+        eslintrc: {
+          enabled: true
+        }
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+        resolvers: [
+          AntDesignVueResolver({
+            resolveIcons: true,
+            importStyle: false,
+          }),
+        ],
+        dirs: ['src/components']
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        symbolId: "icon-[dir]-[name]",
+        svgoOptions: command === "build",
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 后端服务地址
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080', // 后端服务地址
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
